5c77524fbdf82f03c60329edb4a772e62ae41df0
niranjan-2724 <iamniranjanxyz27>
2026-06-20 14:41:20 +0530

    render deploy

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'iamniranjanxyz27@gmail.com'
-app.config['MAIL_PASSWORD'] = 'wdyl fcpt vtku uxqp'
+app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
 import re
 mail = Mail(app)
 
